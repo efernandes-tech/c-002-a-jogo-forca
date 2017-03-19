faeb38f95f9f2503092eb3e6b76650ebf0f7cbae
edersonlrf <edersonluis.rf@gmail.com>
2017-03-19 10:30:09 -0300

    A2 manipulando arrays

--- forca.c
+++ forca.c
@@ -1,13 +1,52 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-	char letra1 = 'M';
-	char letra2 = 'F';
-	char letra3 = 'A';
+	char palavrasecreta[20];
+	
+	sprintf(palavrasecreta, "MELANCIA"); // Ele escreve "MELANCIA\0", o "\0" informa que acabou a palavra.
+	
+	int acertou = 0;
+	int enforcou = 0;
+	
+	char chutes[26];
+	int tentativas = 0;
+	
+	do {
+		int i, j;
 
-	printf("%c %c %c\n", letra1, letra2, letra3);
+		for (i = 0; i < strlen(palavrasecreta); i++) {
+			int achou = 0;
+			
+			for (j = 0; j < tentativas; j++) {
+				if (chutes[j] == palavrasecreta[i]) {
+					achou = 1;
+					break;
+				}
+			}
+			
+			if (achou) {
+				printf("%c ", palavrasecreta[i]);
+			} else {
+				printf("_ ");
+			}
+		}
+		printf("\n");
+		
+		char chute;
+		
+		printf("Qual letra?");
+		scanf(" %c", &chute); // O espaço " " antes do "%c" resolve o problema do enter.
 
-	printf("teste\n");
+		chutes[tentativas] = chute;
+		tentativas++;
+
+		for (i = 0; i < strlen(palavrasecreta); i++) {
+			if (palavrasecreta[i] == chute) {
+				printf("A posição %d tem essa letra\n", i+1);
+			}
+		}
+	} while (!acertou && !enforcou);
 	
 	system("pause");
 }
