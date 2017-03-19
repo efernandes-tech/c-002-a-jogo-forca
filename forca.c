7c054df74e9e146be9d5e2d309f901a5244ecb61
edersonlrf <edersonluis.rf@gmail.com>
2017-03-19 09:37:56 -0300

    correcao

---
+++ forca.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	char letra1 = 'M';
+	char letra2 = 'F';
+	char letra3 = 'A';
+
+	printf("%c %c %c\n", letra1, letra2, letra3);
+
+	printf("teste\n");
+	
+	system("pause");
+}
