5ba09c2979089a6992761a277cdc5b008e64b0a4
edersonlrf <edersonluis.rf@gmail.com>
2017-03-26 10:31:28 -0300

    a05 arrays e ponteiros

--- forca.c
+++ forca.c
@@ -1,69 +1,60 @@
 #include <stdio.h>
 #include <string.h>
 
-char palavrasecreta[20];
-char chutes[26];
-int tentativas = 0;
-
 void abertura() {
-    printf("/*****************/\n");
-    printf("/* Jogo de Forca */\n");
-    printf("/*****************/\n\n");
+	printf("/*****************/\n");
+	printf("/* Jogo de Forca */\n");
+	printf("/*****************/\n\n");
 }
 
-void chuta(char chutes[], int tentativas) {
-    char chute;
-		
-	printf("Qual letra?");
+void chuta(char chutes[], int* tentativas) { // Declaracao de ponteiro.
+	char chute;
+
+	printf("Qual letra? ");
 	scanf(" %c", &chute);
 
-	chutes[tentativas] = chute;
-	tentativas++;
-}
-
-int jachutou(char letra) {
-    int achou = 0;
-    int j;
-    for (j = 0; j < tentativas; j++) {
-        if (chutes[j] == letra) {
-            achou = 1;
-            break;
-        }
-    }
-    return achou;
-}
-
-void desenhaforca() {
-    printf("Você já deu %d chutes\n", tentativas);
-	int i;
-    for (i = 0; i < strlen(palavrasecreta); i++) {
-        if (jachutou(palavrasecreta[i])) {
-            printf("%c ", palavrasecreta[i]);
-        } else {
-            printf("_ ");
-        }
-    }
-    printf("\n");
-}
-
-void escolhepalavra() {
-    sprintf(palavrasecreta, "MELANCIA");
+	// 
+	chutes[*tentativas] = chute; // Usando o ponteiro.
+	(*tentativas)++;
 }
 
 int main() {
+	char palavrasecreta[20];
+	sprintf(palavrasecreta, "MELANCIA");
+
 	int acertou = 0;
 	int enforcou = 0;
 
+	char chutes[26];
+	int tentativas = 0;
+
 	abertura();
-	escolhepalavra();
 
 	do {
-		desenhaforca();
+		printf("Você já deu %d chutes\n", tentativas);
 
-		chuta(chutes, tentativas);
+		int i;
+		for(i = 0; i < strlen(palavrasecreta); i++) {
+			int achou = 0;
 
-        tentativas++;		
+			int j;
+			for(j = 0; j < tentativas; j++) {
+				if(chutes[j] == palavrasecreta[i]) {
+					achou = 1;
+					break;
+				}
+			}
+
+			if(achou) {
+				printf("%c ", palavrasecreta[i]);
+			} else {
+				printf("_ ");
+			}
+		}
+		printf("\n");
+
+		// Passagem de parametro por referencia (ponteiro) e nao por copia.
+		chuta(chutes, &tentativas);
 	} while (!acertou && !enforcou);
-	
-	system("pause");
+
 }
